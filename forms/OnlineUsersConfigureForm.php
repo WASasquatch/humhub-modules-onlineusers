c08f13709c5a697c9a0b30efe5fcb2e0c8ca5655
Jordan Thompson <jordanslost@gmail.com>
2015-05-09 23:55:19 -0700

    Update OnlineUsersConfigureForm.php

--- forms/OnlineUsersConfigureForm.php
+++ forms/OnlineUsersConfigureForm.php
@@ -12,24 +12,24 @@ class OnlineUsersConfigureForm extends CFormModel {
 
     public function rules() {
         return array(
-            array('limit', 'required', 'message'=>Yii::t('onlineusers.views_config', 'Please fill out all required fields.')),
-        	array('limit', 'compare', 'compareValue'=>'10', 'operator'=>'>=', 'message'=> Yii::t('onlineusers.views_config', '<strong>10</strong> or more users must be shown at once.')),
-            array('sortOrder', 'required', 'message'=>Yii::t('onlineusers.views_config', 'Please fill out all required fields.')),
-        	array('sortOrder', 'compare', 'compareValue'=>'100', 'operator'=>'>=', 'message'=> Yii::t('onlineusers.views_config', 'Sort order must be greater then <strong>100</strong>')),
-            array('adminBorderColor', 'required', 'message'=>Yii::t('onlineusers.views_config', 'Please fill out all required fields.')),
-            array('adminBorderColor', 'match', 'pattern'=>'/^#[0-9a-f]{3,6}$/i', 'message'=>Yii::t('onlineusers.views_config', 'Please only enter hex color codes.')),
-            array('borderColor', 'required', 'message'=>Yii::t('onlineusers.views_config', 'Please fill out all required fields.')),
-            array('borderColor', 'match', 'pattern'=>'/^#[0-9a-f]{3,6}$/i', 'message'=>Yii::t('onlineusers.views_config', 'Please only enter hex color codes.'))
+            array('limit', 'required', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please fill out all required fields.')),
+        	array('limit', 'compare', 'compareValue'=>'10', 'operator'=>'>=', 'message'=> Yii::t('OnlineUsersModule.views_config', '<strong>10</strong> or more users must be shown at once.')),
+            array('sortOrder', 'required', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please fill out all required fields.')),
+        	array('sortOrder', 'compare', 'compareValue'=>'100', 'operator'=>'>=', 'message'=> Yii::t('OnlineUsersModule.views_config', 'Sort order must be greater then <strong>100</strong>')),
+            array('adminBorderColor', 'required', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please fill out all required fields.')),
+            array('adminBorderColor', 'match', 'pattern'=>'/^#[0-9a-f]{3,6}$/i', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please only enter hex color codes.')),
+            array('borderColor', 'required', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please fill out all required fields.')),
+            array('borderColor', 'match', 'pattern'=>'/^#[0-9a-f]{3,6}$/i', 'message'=>Yii::t('OnlineUsersModule.views_config', 'Please only enter hex color codes.'))
 
             );
     }
 
     public function attributeLabels() {
         return array(
-            'limit' => Yii::t('onlineusers.views_config', 'Online Users to Show'),
-            'sortOrder' => Yii::t('onlineusers.views_config', 'Widget Sort Order <i>(ex: 300)</i>'),
-            'adminBorderColor' => Yii::t('onlineusers.views_config', 'Admin Border Color (Hex Code)'),
-            'borderColor' => Yii::t('onlineusers.views_config', 'User Border Color')
+            'limit' => Yii::t('OnlineUsersModule.views_config', 'Online Users to Show'),
+            'sortOrder' => Yii::t('OnlineUsersModule.views_config', 'Widget Sort Order <i>(ex: 300)</i>'),
+            'adminBorderColor' => Yii::t('OnlineUsersModule.views_config', 'Admin Border Color (Hex Code)'),
+            'borderColor' => Yii::t('OnlineUsersModule.views_config', 'User Border Color')
         );
     }
 
